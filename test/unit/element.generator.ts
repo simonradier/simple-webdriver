a3114c930d29b3525c59b5681fa6a8c94c48cd5f
Critik <critikkiller@gmail.com>
2021-06-20 22:05:02 +0200

    Add screenshot tests

--- test/unit/element.generator.ts
+++ test/unit/element.generator.ts
@@ -314,6 +314,22 @@ export function generateElementTest(browserType : string) {
             });
         });
 
-   
+        describe('screenshot', function () {
+            it('should return the image base 64 of the element is enabled if webdriver response is successful', async function () {
+                let element : Element = await g_browser.findElement(Using.tag, "input");
+                let resp = td.WD_ELEMENT_SCREENSHOT.OK;
+                nock(td.WD_SERVER_URL_HTTP[browserType]).get(`/session/${td.WD_SESSION_ID}/element/${td.WD_ELEMENT_ID}/screenshot`).reply(resp.code, resp.body, resp.headers);
+                await expect(element.screenshot()).to.be.fulfilled;
+            });
+
+            it('should throw an error if the webdriver server return an error ', async function () {
+                let element : Element = await g_browser.findElement(Using.tag, "h1");
+                let resp = td.WD_ELEMENT_SCREENSHOT.KO_NOT_FOUND;
+                //@ts-ignore
+                element['element-6066-11e4-a52e-4f735466cecf'] = td.WD_ELEMENT_ID_FAKE;
+                nock(td.WD_SERVER_URL_HTTP[browserType]).get(`/session/${td.WD_SESSION_ID}/element/${td.WD_ELEMENT_ID}/enabled`).reply(resp.code, resp.body, resp.headers);
+                await expect(element.screenshot()).to.be.rejected;
+            });
+        });   
     });
 }
